a4df8b3c7abe4738871759cea1023e4bd06717a4
iredstonne <iredstonne.dev@gmail.com>
2024-04-23 23:47:51 +0200

    Make image class disposable

--- src/main/java/io/github/engine/image/Image.java
+++ src/main/java/io/github/engine/image/Image.java
@@ -2,40 +2,43 @@ package io.github.engine.image;
 
 import java.nio.ByteBuffer;
 
-import io.github.engine.renderer.RenderSystem;
+import io.github.engine.util.Disposable;
+import lombok.Getter;
 import org.lwjgl.BufferUtils;
 
-import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL30.*;
+import static org.lwjgl.system.MemoryUtil.*;
 
-public class Image {
+@Getter
+public class Image implements Disposable {
 
     private final int width;
     private final int height;
     private final ByteBuffer pixels;
+    private int textureId;
 
     public Image(int width, int height) {
         this.width = width;
         this.height = height;
         this.pixels = BufferUtils.createByteBuffer((this.width * this.height * 3));
+        this.upload();
     }
 
-    public int createTexture() {
-        final int texturesId = RenderSystem.genTextures();
-        RenderSystem.bindTexture(GL_TEXTURE_2D, texturesId);
-        RenderSystem.texParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-        RenderSystem.texParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
-        RenderSystem.texParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-        RenderSystem.texParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-        RenderSystem.texImage2D(GL_TEXTURE_2D, GL_RGB, GL_BYTE, this.width, this.height, this.pixels.flip());
-        return texturesId;
+    public void upload() {
+        this.textureId = glGenTextures();
+        glBindTexture(GL_TEXTURE_2D, textureId);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexImage2D(GL_TEXTURE_2D, (int) NULL, GL_RGB, this.width, this.height, (int) NULL, GL_RGB, GL_BYTE, this.pixels.flip());
     }
 
-    public int getWidth() {
-        return this.width;
-    }
-
-    public int getHeight() {
-        return this.height;
+    @Override
+    public void dispose() {
+        if(this.textureId != NULL) {
+            glDeleteTextures(this.textureId);
+        }
     }
 
 }
